145eba75bd8a792ae1aca7795d40bbf169412944
windjay <windjay@daum.net>
2019-02-03 21:06:26 +0900

    userController mock test 추가

--- src/main/java/com/api/UserController.java
+++ src/main/java/com/api/UserController.java
@@ -1,6 +1,7 @@
 package com.api;
 
 import com.domain.User;
+import com.repository.UserRepository;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -18,10 +20,21 @@ public class UserController {
     @Autowired
     UserService userService;
 
+//    @Autowired
+//    UserRepository userRepository;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable String id, HttpServletRequest req) {
         User user = userService.findById(id).orElseThrow(() -> new RuntimeException("Not Found " + id));
         return ResponseEntity.ok(user);
     }
+
+//    @GetMapping("/{id}")
+//    public ResponseEntity<User> findById(@PathVariable String id, HttpServletRequest req) {
+//        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Not Found " + id));
+//        return ResponseEntity.ok(user);
+//    }
+
+
 
 }
