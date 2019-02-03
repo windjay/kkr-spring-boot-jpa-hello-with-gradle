fd8e5eafc5f99b8dd78cc3951a5b306c0dfef739
windjay <windjay@daum.net>
2019-02-03 21:06:26 +0900

    controller mock test 추가

--- src/test/java/com/api/UserControllerTest.java
+++ src/test/java/com/api/UserControllerTest.java
@@ -3,19 +3,23 @@ package com.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(UserController.class)
+//@WebMvcTest(UserController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
   @Autowired
@@ -26,9 +30,10 @@ public class UserControllerTest {
     mockMvc.perform(get("/user/windjay"))
             .andExpect(status().isOk())
             .andDo(print())
-            .andExpect(view().name("hello"))
-            .andExpect(model().attribute("name", is("windjay")))
-            .andExpect(xpath("//h1").string("windjay")) // xml path
+//            .andExpect(view().name("hello"))
+            .andExpect(jsonPath("$.name", is(equalTo("windjay"))))
+            .andExpect(jsonPath("$.id", is(equalTo("windjay"))))
+//            .andExpect(xpath("//h1").string("windjay")) // xml path
             .andExpect(content().string(containsString("windjay")));
   }
 
